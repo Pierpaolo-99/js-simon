fc7b6e8efab7f603b9e2cd13153d06c583d7e5e8
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-02-20 14:56:42 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+/* Descrizione:
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+NOTA:
+non è importante l'ordine con cui l'utente inserisce i numeri, basta che ne indovini il più possibile. */
+
+// take the elements and save in the variable
+
+const countdownEl = document.getElementById('countdown')
+console.log(countdownEl);
+
+const numbersListEl = document.getElementById('numbers-list')
+console.log(numbersListEl);
+
+const formEl = document.getElementById('answers-form')
+console.log(formEl);
+
+// generate five random numbers
+
+const randomNumbers = Math.floor(Math.random() * 100)
+
+numbersListEl.innerHTML = `<li>${randomNumbers}</li>`
+
+console.log(numbersListEl);
+
+
+
+
+
